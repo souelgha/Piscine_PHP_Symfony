5cdb617853671bf58af938c59897f4a106097b0e
Sonia Ouelghabet <sonouelg@paul-f4Ar8s2.clusters.42paris.fr>
2025-04-02 18:25:49 +0200

    ex05

--- Module0_Starting/ex05/search_by_states.php
+++ Module0_Starting/ex05/search_by_states.php
@@ -1,6 +1,6 @@
 
 <?php
-function search_by_states(string $data)/*: string*/{
+function search_by_states(string $data){
     $stringreturn=[];
 
     $states = [
@@ -16,18 +16,40 @@ function search_by_states(string $data)/*: string*/{
         'KS' => 'Topeka',
     ];
     $items=explode(", ", $data);
+    $index= 0;
     foreach($items as $value)
-    {
-        echo $value, "\n";
-        foreach($states as $etat => $code){
+    {  
+        foreach($states as $etat => $code)
+        {
             if($etat == $value)
             {
-                
-            $stringreturn[i]= $capitals[$code] . "is the capital of ".$value."\n";
-            echo $stringreturn;
-            }
+                $key= array_key_exists($code,$capitals);
+                if(!$key)
+                    $stringreturn[$index++]= $value. " is neither a capital nor a state.\n";
+                else              
+                    $stringreturn[$index++]= $capitals[$code] . " is the capital of ".$value.".\n";
+                break;                
+            }            
         }
+        foreach($capitals as $code => $cap)
+        {
+            if($cap == $value)
+            {
+                $key= array_search($code,$states);
+                if(!$key)
+                    $stringreturn[$index++]= $value. " is neither a capital nor a state.\n";
+                else
+                    $stringreturn[$index++]= $cap . " is the capital of ". $key.".\n"; 
+                break;                
+            }            
+
+        }
+        if(!(array_key_exists($value,$states)) && !(array_search($value,$capitals)))
+            $stringreturn[$index++]= $value. " is neither a capital nor a state.\n";
+
     }
-     // return("Unknown\n");
+    unset($stringreturn[$index]);
+    foreach($stringreturn as $value)
+        echo $value;
 }                      
 
--- Module0_Starting/ex05/test05.php
+++ Module0_Starting/ex05/test05.php
@@ -1,4 +1,4 @@
 <?php
 
 include('./search_by_states.php');
-search_by_states("Oregon, trenton, Topeka, NewJersey");
+search_by_states("Oregon, trenton, Topeka, NewJersey, Colorado");
