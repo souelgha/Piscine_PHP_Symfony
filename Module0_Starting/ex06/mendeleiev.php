89b71dc0639c299fb15ce87d1d801b5c937c59b5
Sonia Ouelghabet <sonouelg@paul-f4Ar8s2.clusters.42paris.fr>
2025-04-04 17:54:45 +0200

    ex06

--- Module0_Starting/ex06/mendeleiev.php
+++ Module0_Starting/ex06/mendeleiev.php
@@ -1,28 +1,87 @@
 <?php
+     $html = "<!DOCTYPE html>\n<html>\n<head>\n<title>Tableau de Mendeleiev</title>\n</head>\n<body>\n";
+     $html .= "<h2>Tableau Périodique des Éléments</h2>\n";
+   
 
-$handle=file_get_contents("ex06-mod.txt");
-// echo $handle;
-$number=explode("\n", $handle);
-print_r($number);
-$element=explode("= ",$number[0] );
-print_r($element);
+    $handle=file_get_contents("ex06-mod.txt");
+    $number=explode("\n", $handle);
+
+    $html .= "<table border='1' style='border-collapse: collapse; text-align: center;'>\n";
+    /* boucle ici
+    position correspond au groupe
+    quand le group = 17, on incremente la periode
+
+    */
+    $period_max=7;
+    $group_max=18;
+    // $index = 0; //position
+    for($period = 0; $period <$period_max; $period++)
+    $html .= "<tr>";
+    for($group = 0; $group < $group_max ; $group++)
+    {
+        foreach($number as $value)
+        {
+            $line = $value;
+            echo $line;
+            preg_match('/^(\w+) = (.+)$/', $line, $matches);
+            if(!$matches){
+                die("format incorrect");
+            }
+            $name= $matches[1];
+            $data = $matches[2];
+            // echo $name."\n";
+            // echo $data."\n";
+            $dataextract=explode(", ", $data);
+            $composition=[];
+            // print_r($dataextract);
+            $composition["name"]=$name;
+            foreach($dataextract as $value)
+            {
+                list($key,$val)=explode(":",$value);
+                $composition[$key]=$val;
+            }
+            if($composition[position] == $group)
+            {
+                $html .= "<td>\n";
+                $html .= "<h4>{$composition[name]}</h4>\n";
+                $html .=  "<ul>\n";
+                $html .=   "<li>{$composition[number]}</li>\n";
+                $html .=   "<li>{$composition[small]}</li>\n";
+                $html .=   "<Li>{$composition[molar]} </ li>\n";
+                $html .=   "<li>{$composition[electron]}</li>\n";
+                $html .=   "<ul>\n";
+                $html .=   "</td>\n";
+            }
+            else
+            {
+                $html .= "<td></td>\n";
+            }
 
 
-// foreach($number as $value)
-// {
-//     $element[]
-// }
+            // print_r($composition);
+        }
+    }
+
+
+$html .= "<td>\n";
+                $html .= "<b>{$el['symbol']}</b><br>\n";
+                $html .= "<small>{$el['name']} ({$el['num']})</small>\n";
+                $html .= "<ul>\n";
+                $html .= "<li>Masse: {$el['mass']}</li>\n";
+                $html .= "</ul>\n</td>\n";
+                $found = true;
+                break;
 
 
 
 // <table>
-// <tr>
-//     <td style="border: 1px solid black; padding:10px">
-//         <h4>Hydrogen</h4>
-//             <ul>
-//                 <li>No 42</li>
-//                 <li>H</li>
-//                 <Li> 1.00794 </ li>
-//                 <li>1 electron</li>
-//             <ul>
-//     </td>
+<tr>
+$html .= "<td style="border: 1px solid black; padding:10px">\n"
+$html .= "<h4>{$composition[name]}</h4>\n"
+$html .=  "<ul>\n"
+$html .=   "<li>{$composition[number]}</li>\n"
+$html .=   "<li>{$composition[small]}</li>\n"
+$html .=   "<Li>{$composition[molar]} </ li>\n"
+$html .=   "<li>{$composition[electron]}</li>\n"
+$html .=   "<ul>\n"
+$html .=   "</td>\n"
