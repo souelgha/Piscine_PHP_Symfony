a40676f3821d08b516d970c178ee614cbcf04f86
ouelghabet <souelghabet@gmail.com>
2025-04-09 12:04:40 +0200

    oob

---
+++ Module0_Oob/ex00/TemplateEngine.php
@@ -0,0 +1,14 @@
+<?php
+
+class TemplateEngine
+{
+    public $fileName="unknown";
+    public $templateName="";
+    public $parameters=[];
+
+    public function createFile($fileName, $templateName, $parameters)
+    {
+
+    }
+
+}
