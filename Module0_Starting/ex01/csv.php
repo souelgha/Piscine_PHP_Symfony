2c0601ae2fa6d022b41f301e9c6df184c66ed928
sonia <sonia@debian13-Symfony.myguest.virtualbox.org>
2026-02-07 18:34:34 +0100

    small modification on starting

--- Module0_Starting/ex01/csv.php
+++ Module0_Starting/ex01/csv.php
@@ -1,10 +1,16 @@
 <?php
 
 $handle=file_get_contents("ex01.txt");
-// echo $handle;
-$number=explode(",", $handle);
-echo $number[0], "\n";
-echo $number[1], "\n";
-echo $number[2], "\n";
-echo $number[3], "\n";
+echo $handle;
+$number=explode(",", $handle); // permet de split en tableau une str
 
+foreach($number as $value){
+    
+    if($value == end($number)){
+         echo $value;
+    }else{
+        echo $value, "\n";
+    }
+   
+}
+?>
