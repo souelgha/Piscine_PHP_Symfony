15a813b277c0c10d86c281b0e989cf4049e02d7c
Sonia OUELGHABET <souelghabet@gmail.com>
2026-02-28 16:56:27 +0100

    update 20260228

--- Module0_Starting/ex05/search_by_states.php
+++ Module0_Starting/ex05/search_by_states.php
@@ -1,9 +1,6 @@
 
 <?php
-function search_by_states(string $data){
-    $stringreturn=[];
-
-    $states = [
+ $states = [
         'Oregon' => 'OR',
         'Alabama' => 'AL',
         'New Jersey' => 'NJ',
@@ -15,41 +12,78 @@ function search_by_states(string $data){
         'NJ' => 'trenton',
         'KS' => 'Topeka',
     ];
+function search_by_states(string $data){
+	global $states, $capitals;
+    $stringToReturn=[];
+   
     $items=explode(", ", $data);
     $index= 0;
-    foreach($items as $value)
-    {  
-        foreach($states as $etat => $code)
-        {
-            if($etat == $value)
-            {
-                $key= array_key_exists($code,$capitals);
-                if(!$key)
-                    $stringreturn[$index++]= $value. " is neither a capital nor a state.\n";
-                else              
-                    $stringreturn[$index++]= $capitals[$code] . " is the capital of ".$value.".\n";
-                break;                
-            }            
-        }
-        foreach($capitals as $code => $cap)
-        {
-            if($cap == $value)
-            {
-                $key= array_search($code,$states);
-                if(!$key)
-                    $stringreturn[$index++]= $value. " is neither a capital nor a state.\n";
-                else
-                    $stringreturn[$index++]= $cap . " is the capital of ". $key.".\n"; 
-                break;                
-            }            
+    
+	foreach($items as $value){
+		if(isset($states[$value])){
+			if(isset($capitals[$states[$value]])){
+				$stringToReturn[$index++]= $capitals[$states[$value]] . " is the capital of ".$value.".\n";
+			}else{
+				$stringToReturn[$index++]= $value. " is neither a capital nor a state.\n";
+			}
+		}else if(array_search($value,$capitals)){
+			$symb=array_search($value,$capitals);
+			if(array_search($symb,$states)){
+				$stringToReturn[$index++]= $value . " is the capital of ". array_search($symb,$states) . ".\n";		
+			}else{
+				$stringToReturn[$index++]= $value. " is neither a capital nor a state.\n";
+			}
+		}else if(!(isset($states[$value])) && (!array_search($value,$capitals))){
+			$stringToReturn[$index++]= $value. " is neither a capital nor a state.\n";
+		}
+	}
+	foreach($stringToReturn as $part){
+		echo $part;
+	};
+	
+}   
+// array_seache => permet de trouver la 1ere key d un tableau a partir de sa valeur                  
+// function search_by_states(string $data){
+// 	global $states, $capitals;
+//     $stringreturn=[];
 
-        }
-        if(!(array_key_exists($value,$states)) && !(array_search($value,$capitals)))
-            $stringreturn[$index++]= $value. " is neither a capital nor a state.\n";
+   
+//     $items=explode(", ", $data);
+//     $index= 0;
+//     foreach($items as $value)
+//     {  
+//         foreach($states as $etat => $code)
+//         {
+//             if($etat == $value)
+//             {
+//                 $key= array_key_exists($code,$capitals);
+//                 if(!$key)
+//                     $stringreturn[$index++]= $value. " is neither a capital nor a state.\n";
+//                 else              
+//                     $stringreturn[$index++]= $capitals[$code] . " is the capital of ".$value.".\n";
+//                 break;                
+//             }            
+//         }
+//         foreach($capitals as $code => $cap)
+//         {
+//             if($cap == $value)
+//             {
+//                 $key= array_search($code,$states);
+//                 if(!$key)
+//                     $stringreturn[$index++]= $value. " is neither a capital nor a state.\n";
+//                 else
+//                     $stringreturn[$index++]= $cap . " is the capital of ". $key.".\n"; 
+//                 break;                
+//             }            
 
-    }
-    unset($stringreturn[$index]);
-    foreach($stringreturn as $value)
-        echo $value;
-}                      
+//         }
+//         if(!(array_key_exists($value,$states)) && !(array_search($value,$capitals)))
+//             $stringreturn[$index++]= $value. " is neither a capital nor a state.\n";
+
+//     }
+//     unset($stringreturn[$index]);
+//     foreach($stringreturn as $value)
+//         echo $value;
+// }                      
+
 
